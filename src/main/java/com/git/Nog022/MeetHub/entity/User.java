6bdfe3e7bddaeb2f59ae63678fb2b6f05c5c6991
Rodrigo Nogueira <nogueiragm22@hotmail.com>
2024-09-21 00:17:31 -0300

    ajustando User.java

--- src/main/java/com/git/Nog022/MeetHub/entity/User.java
+++ src/main/java/com/git/Nog022/MeetHub/entity/User.java
@@ -1,15 +1,26 @@
 package com.git.Nog022.MeetHub.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "USER")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
+    private Integer id;
+    @Column(name = "NAME", length = 100)
     private String name;
+    @Column(name = "EMAIL", length = 100)
     private String email;
-    private Integer cpf;
+    @Column(name = "CPF", length = 11)
+    private String cpf;
+    @Column(name = "PASSWORD", length = 100)
     private String password;
+    @Column(name = "COMPANYNAME", length = 100)
     private String companyName;
 
 }
