1e2e957bed6fb1ca860d80c06cb5e0cfaa6c2f7c
Rodrigo Nogueira <nogueiragm22@hotmail.com>
2024-09-21 00:31:44 -0300

    colocando id nas classes

--- src/main/java/com/git/Nog022/MeetHub/entity/Reservation.java
+++ src/main/java/com/git/Nog022/MeetHub/entity/Reservation.java
@@ -1,13 +1,15 @@
 package com.git.Nog022.MeetHub.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 import java.time.LocalDateTime;
-
-@Entity
 @Data
+@Entity
 public class Reservation {
+    @Id
+    private Integer id;
     private String personName;
     private String chosenRoom;
     private LocalDateTime date;
--- src/main/java/com/git/Nog022/MeetHub/entity/Room.java
+++ src/main/java/com/git/Nog022/MeetHub/entity/Room.java
@@ -1,11 +1,13 @@
 package com.git.Nog022.MeetHub.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
-
-@Entity
 @Data
+@Entity
 public class Room {
+    @Id
+    private Integer id;
     private String name;
     private Integer capacity;
     private String place;
