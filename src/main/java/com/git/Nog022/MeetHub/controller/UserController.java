5b07bd1efa96cf45945a5880229786fa7a9890d5
Rodrigo Nogueira <nogueiragm22@hotmail.com>
2024-10-03 08:33:43 -0300

    criando o metodo de update do user

--- src/main/java/com/git/Nog022/MeetHub/controller/UserController.java
+++ src/main/java/com/git/Nog022/MeetHub/controller/UserController.java
@@ -2,6 +2,8 @@ package com.git.Nog022.MeetHub.controller;
 
 import com.git.Nog022.MeetHub.entity.User;
 import com.git.Nog022.MeetHub.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
@@ -13,6 +15,8 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/api/users")
 public class UserController {
+    public static Logger logger = LoggerFactory.getLogger(UserController.class);
+
 
     @Autowired
     UserRepository userRepository;
@@ -29,6 +33,24 @@ public class UserController {
             userRepository.delete(userId);
             return userId;
         }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuario nao encontrado"));
+
+
+    }
+
+    @PutMapping("/update/{id}")
+    public void update(@PathVariable Integer id,@RequestBody @Validated User user){
+
+        User userOtional = userRepository.findById(user.getId()).map(
+                userFind -> {
+                    user.setId(userFind.getId());
+                    user.setCpf(userFind.getCpf());
+                    user.setName(userFind.getName());
+                    user.setEmail(userFind.getEmail());
+                    user.setPassword(userFind.getPassword());
+                    user.setCompanyName(userFind.getCompanyName());
+                    return userFind;
+                }
+        ).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuario nao encontrado"));
 
 
     }
