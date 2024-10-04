8c2efa77f78085159f326b2ac1ef8a721c8ca163
Rodrigo Nogueira <nogueiragm22@hotmail.com>
2024-10-04 20:06:35 -0300

    ajustando a logica do controller

--- src/main/java/com/git/Nog022/MeetHub/controller/UserController.java
+++ src/main/java/com/git/Nog022/MeetHub/controller/UserController.java
@@ -1,14 +1,13 @@
 package com.git.Nog022.MeetHub.controller;
 
 import com.git.Nog022.MeetHub.entity.User;
-import com.git.Nog022.MeetHub.repository.UserRepository;
+import com.git.Nog022.MeetHub.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -17,22 +16,19 @@ import java.util.List;
 public class UserController {
     public static Logger logger = LoggerFactory.getLogger(UserController.class);
 
-
     @Autowired
-    UserRepository userRepository;
+    private UserService userService;
 
     @PostMapping("/save")
     @ResponseStatus(HttpStatus.CREATED)
     public User save(@RequestBody @Validated User user){
-        return userRepository.save(user);
+        logger.info("entrou save");
+        return userService.save(user);
     }
 
     @DeleteMapping("/delete/{id}")
     public void delete(@PathVariable Integer id){
-        userRepository.findById(id).map(userId -> {
-            userRepository.delete(userId);
-            return userId;
-        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuario nao encontrado"));
+        userService.delete(id);
 
 
     }
@@ -40,20 +36,13 @@ public class UserController {
     @PutMapping("/update/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(@PathVariable Integer id,@RequestBody @Validated User user){
-
-        userRepository.findById(id).map(
-                userFind -> {
-                    user.setId(userFind.getId());
-                    userRepository.save(user);
-                    return userFind;
-                }
-        ).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuario nao encontrado"));
+        userService.update(id,user);
 
     }
 
     @GetMapping("/listAll")
     public List<User> listUser(){
-        return userRepository.findAll();
+        return userService.listUser();
     }
 
 }
