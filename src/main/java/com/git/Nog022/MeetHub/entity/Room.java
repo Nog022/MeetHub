0bf412309ee9abefa7f042e2865eca5cf5908d8f
Rodrigo Nogueira <nogueiragm22@hotmail.com>
2024-10-04 20:07:21 -0300

    inseridno nova mensagem no messages.properties e ajustando classe room

--- src/main/java/com/git/Nog022/MeetHub/entity/Room.java
+++ src/main/java/com/git/Nog022/MeetHub/entity/Room.java
@@ -1,19 +1,25 @@
 package com.git.Nog022.MeetHub.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 @Data
 @Entity
+@Table(name = "ROOMS")
 public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "ROOM_ID")
     private Integer id;
     @Column(name = "name_room", length = 255)
+    @NotEmpty(message = "{field.required}")
     private String name;
     @Column(name = "capacity", length = 255)
+    @NotNull(message = "{capacity.null}")
     private Integer capacity;
     @Column(name = "place", length = 255)
+    @NotEmpty(message = "{field.required}")
     private String place;
     @Column(name = "resources", length = 255)
     private String resources;
