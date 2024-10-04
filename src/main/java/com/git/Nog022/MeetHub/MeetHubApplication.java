afc08ff68c9c2a5da4e8ee5dd0c0908d120a9cf8
Rodrigo Nogueira <nogueiragm22@hotmail.com>
2024-10-04 20:04:15 -0300

    inseirndo um ComponentScan no application

--- src/main/java/com/git/Nog022/MeetHub/MeetHubApplication.java
+++ src/main/java/com/git/Nog022/MeetHub/MeetHubApplication.java
@@ -3,8 +3,10 @@ package com.git.Nog022.MeetHub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@ComponentScan
 public class MeetHubApplication {
 
 	public static void main(String[] args) {
