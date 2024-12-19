f6ba3d78924e94bc93df575d3fbecf78a4b59594
Rodrigo Nogueira <nogueiragm22@hotmail.com>
2024-12-19 11:29:19 -0300

    configurando o application e comentando classe do security

--- src/main/java/com/git/Nog022/MeetHub/MeetHubApplication.java
+++ src/main/java/com/git/Nog022/MeetHub/MeetHubApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-@ComponentScan
 public class MeetHubApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/git/Nog022/MeetHub/config/SecurityConfig.java
+++ src/main/java/com/git/Nog022/MeetHub/config/SecurityConfig.java
@@ -1,59 +1,59 @@
-package com.git.Nog022.MeetHub.config;
-
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-@EnableMethodSecurity
-@Slf4j
-public class SecurityConfig  {
-    public static Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
-    @Bean
-    public InMemoryUserDetailsManager userDatailService(PasswordEncoder passwordEncoder){
-        UserDetails user = User.withUsername("user")
-                .password(passwordEncoder.encode("password1"))
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.withUsername("admin")
-                .password(passwordEncoder.encode("admin"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
-
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
-        return httpSecurity.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(request -> request.anyRequest()
-                .authenticated())
-                .httpBasic(Customizer.withDefaults())
-                .build();
-
-
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-
-
-}
+//package com.git.Nog022.MeetHub.config;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableMethodSecurity
+//@Slf4j
+//public class SecurityConfig  {
+//    public static Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
+//    @Bean
+//    public InMemoryUserDetailsManager userDatailService(PasswordEncoder passwordEncoder){
+//        UserDetails user = User.withUsername("user")
+//                .password(passwordEncoder.encode("password1"))
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User.withUsername("admin")
+//                .password(passwordEncoder.encode("admin"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user, admin);
+//
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+//        return httpSecurity.csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(request -> request.anyRequest()
+//                .authenticated())
+//                .httpBasic(Customizer.withDefaults())
+//                .build();
+//
+//
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//    }
+//
+//
+//
+//}
