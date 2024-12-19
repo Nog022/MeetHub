1bec4972ece6a9dcca94c079c9554c26d25b71ff
Rodrigo Nogueira <nogueiragm22@hotmail.com>
2024-12-19 14:52:58 -0300

    criando o repository de Local

---
+++ src/main/java/com/git/Nog022/MeetHub/repository/LocalRepository.java
@@ -0,0 +1,9 @@
+package com.git.Nog022.MeetHub.repository;
+
+import com.git.Nog022.MeetHub.entity.Local;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LocalRepository extends JpaRepository<Local, Long> {
+}
