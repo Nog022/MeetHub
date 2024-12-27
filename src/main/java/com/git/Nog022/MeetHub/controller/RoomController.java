77deb39c53be39a3b7c3112143b1cd5b0b5f97c8
Rodrigo Nogueira <nogueiragm22@hotmail.com>
2024-12-27 17:03:36 -0300

    configurando os controllers para o swagger

--- src/main/java/com/git/Nog022/MeetHub/controller/RoomController.java
+++ src/main/java/com/git/Nog022/MeetHub/controller/RoomController.java
@@ -2,6 +2,8 @@ package com.git.Nog022.MeetHub.controller;
 
 import com.git.Nog022.MeetHub.entity.Room;
 import com.git.Nog022.MeetHub.service.RoomService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
@@ -11,31 +13,37 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/rooms")
+@Tag(name = "Room", description = "Meeting room management")
 public class RoomController {
 
     @Autowired
     private RoomService roomService;
 
+
     @PostMapping("/save")
     @ResponseStatus(HttpStatus.CREATED)
-    public Room save(@RequestBody @Validated Room room){
+    @Operation(summary = "Create a new room", description = "Creates a new room entry in the system")
+    public Room save(@RequestBody @Validated Room room) {
         return roomService.save(room);
     }
 
     @GetMapping("/listRoom")
-    public List<Room> listRoom(){
+    @Operation(summary = "List all rooms", description = "Returns a list of all registered rooms")
+    public List<Room> listRoom() {
         return roomService.listRoom();
     }
 
     @PutMapping("/update/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void update(@PathVariable Integer id, @RequestBody @Validated Room room){
-        roomService.update(id,room);
+    @Operation(summary = "Update room details", description = "Updates the details of an existing room by its ID")
+    public void update(@PathVariable Integer id, @RequestBody @Validated Room room) {
+        roomService.update(id, room);
     }
 
     @DeleteMapping("/delete/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Integer id){
+    @Operation(summary = "Delete a room", description = "Deletes a room by its ID")
+    public void delete(@PathVariable Integer id) {
         roomService.delete(id);
     }
 }
